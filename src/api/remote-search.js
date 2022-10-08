a07cf74ff5279dbdc660a961863779a1ed9ffcac
mengjoel <2274729213@qq.com>
2022-10-08 14:46:30 +0800

    实现登录

--- src/api/remote-search.js
+++ src/api/remote-search.js
@@ -9,9 +9,23 @@ export function searchUser(name) {
 }
 
 export function transactionList(query) {
-  return request({
-    url: '/vue-element-admin/transaction/list',
-    method: 'get',
-    params: query
-  })
+  // return request({
+  //   url: '/vue-element-admin/transaction/list',
+  //   method: 'get',
+  //   params: query
+  // })
+  const data = {
+    code: 20000,
+    data: {
+      total: 20,
+      'items|20': [{
+        order_no: '@guid()',
+        // timestamp: +Mock.Random.date('T'),
+        username: '@name()',
+        price: '@float(1000, 15000, 0, 2)',
+        'status|1': ['success', 'pending']
+      }]
+    }
+  }
+  return new Promise(resolve => { resolve(data) })
 }
--- src/api/user.js
+++ src/api/user.js
@@ -2,23 +2,35 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/vue-element-admin/user/login',
+    url: '/user/login',
     method: 'post',
     data
   })
 }
 
 export function getInfo(token) {
-  return request({
-    url: '/vue-element-admin/user/info',
-    method: 'get',
-    params: { token }
+  // return request({
+  //   url: '/user/info',
+  //   method: 'get',
+  //   params: { token }
+  // })
+  debugger
+  const data = { data: { roles: ['admin'],
+    introduction: 'I am a super administrator',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Super Admin' }}
+  return new Promise((resolve) => {
+    resolve(data)
   })
 }
 
 export function logout() {
-  return request({
-    url: '/vue-element-admin/user/logout',
-    method: 'post'
+  // return request({
+  //   url: '/user/logout',
+  //   method: 'post'
+  // })
+  return new Promise((resolve) => {
+    resolve({ code: 20000,
+      data: 'success' })
   })
 }
